6c5daca79ddd7e92cf0eff6931a7826d3046870e
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-07 16:20:28 -0300

    practica grupal - entendiendo Angular, Git y Github

--- mercadolibro/src/app/app.config.ts
+++ mercadolibro/src/app/app.config.ts
@@ -8,11 +8,4 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
 
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
