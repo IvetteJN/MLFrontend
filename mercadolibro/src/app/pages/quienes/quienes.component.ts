a0090d6bfa9152b37055daa02219eac7ee8ad929
nahir1009 <nahir1009@gmail.com>
2024-05-07 00:06:22 -0300

    Angular Quienes somos cambios

--- mercadolibro/src/app/pages/quienes/quienes.component.ts
+++ mercadolibro/src/app/pages/quienes/quienes.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../shared/footer/footer.component';
+
 
 
 @Component({
   selector: 'app-quienes',
   standalone: true,
-  imports: [CommonModule,FooterComponent,],
+  imports: [CommonModule,],
   templateUrl: './quienes.component.html',
   styleUrl: './quienes.component.css'
 })
