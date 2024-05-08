13ea121f7ee7d970aa8c5fe7d3a72d95340d2ec6
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-05-08 10:32:03 -0300

    componente landing

--- mercadolibro/src/app/app.component.ts
+++ mercadolibro/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 
 
@@ -9,7 +10,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactoComponent, FooterComponent],
+  imports: [RouterOutlet, ContactoComponent, FooterComponent, LandingComponent],
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
 })
