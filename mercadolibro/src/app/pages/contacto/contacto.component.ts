444748259818d4fafa498206435bc3357ac97862
Ivette Nobiltá <ivette.nobilta@hotmail.com>
2024-05-08 17:24:47 -0300

    starting contacto service

--- mercadolibro/src/app/pages/contacto/contacto.component.ts
+++ mercadolibro/src/app/pages/contacto/contacto.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControlName, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ContactoService } from '../../services/contacto.service';
 
 @Component({
   selector: 'app-contacto',
@@ -8,17 +9,16 @@ import { FormBuilder, FormControlName, FormGroup, ReactiveFormsModule, Validator
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
+
 export class ContactoComponent {
 
-  telefono: number = 351456789;
-  ubicacion: string = "Cordoba, Argentina";
-  facebook: string = "Facebook";
-  instagram: string = "Instagram";
-  twitter: string = "Twitter";
-  linkedin: string = "Linkedin";
-
+  datosContacto:any;
   form!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  
+  constructor(private formBuilder: FormBuilder, private ContactoService:ContactoService) {
+
+    this.datosContacto=ContactoService.getDatosContacto();
+
     this.form = this.formBuilder.group(
       {
         nombre: ['', [Validators.required], []],
