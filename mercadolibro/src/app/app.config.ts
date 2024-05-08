c0ae62136cdba5e0f3bac0ae9fd1d556a6c54707
Ivette Jael Nobiltá <100625720+IvetteJN@users.noreply.github.com>
2024-05-07 21:00:26 -0300

    agrego data binding, favicon.ico, corrijo validaciones del form contacto

--- mercadolibro/src/app/app.config.ts
+++ mercadolibro/src/app/app.config.ts
@@ -1,16 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
-
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
