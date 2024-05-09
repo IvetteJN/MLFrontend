d93032805e591a430e49f8e62ed35d7bd2cc84c8
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-09 20:56:36 -0300

    convierto carousel en comoponente y anido en nuestraseleccion

--- mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
+++ mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CarruselComponent } from '../carrusel/carrusel.component';
 
 @Component({
   selector: 'app-nuestraseleccion',
   standalone: true,
-  imports: [],
+  imports: [CarruselComponent],
   templateUrl: './nuestraseleccion.component.html',
   styleUrl: './nuestraseleccion.component.css'
 })
