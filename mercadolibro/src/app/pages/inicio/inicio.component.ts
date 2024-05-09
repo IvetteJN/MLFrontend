666c110980b263150facae6d2299e6446f7b7c07
Franco Orellana <francoR_orellana@outlook.com>
2024-05-09 17:11:57 -0300

    Se vuelven reactivos a los formularios
    Se agrega funcionalidad a los formularios para que se puedan tomar los datos para su futura manipulación

--- mercadolibro/src/app/pages/inicio/inicio.component.ts
+++ mercadolibro/src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  loginFormulario = new FormGroup({
+    usuario: new FormControl(''),
+    contrasenia: new FormControl(''),
+  })
+  
+  enviarDatosLogin():void{
+    console.log(this.loginFormulario.value);
+  }
 
+  registroFormulario = new FormGroup({
+    usuarioRegistro: new FormControl(''),
+    email: new FormControl(''),
+    contraseniaRegistro: new FormControl(''),
+    repetirContrasenia: new FormControl(''),
+  })
+
+  enviarDatosRegistro():void{
+    console.log(this.registroFormulario.value);
+  }
 }
