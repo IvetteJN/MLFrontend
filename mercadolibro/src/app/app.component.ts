9b0ada579d229ceeba92419d463304b3efee2f4f
Virginia Falconi <falconivirginia@gmail.com>
2024-05-09 18:56:34 -0300

    Actualizacion de rama

--- mercadolibro/src/app/app.component.ts
+++ mercadolibro/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { QuienesComponent } from './pages/quienes/quienes.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactoComponent, FooterComponent, QuienesComponent, NavComponent, LandingComponent],
+  styleUrl: './app.component.css',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'mercadolibro';
+  title = 'Mercado Libro';
 }
