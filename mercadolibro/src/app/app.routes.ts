db6b6b721abb1ed474c07072c7b6729eff3ec8f7
Virginia Falconi <falconivirginia@gmail.com>
2024-05-09 20:03:39 -0300

    Categoria
    Creo componente categoria. Al hacer click en catalogo te muestra el buscador y las categorias disponibles.

--- mercadolibro/src/app/app.routes.ts
+++ mercadolibro/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CategoriaComponent } from './pages/categoria/categoria.component';
 
-export const routes: Routes= [];
+export const routes: Routes= [{path: 'categoria', title: 'Categorias', component: CategoriaComponent}];
---
+++ mercadolibro/src/app/pages/categoria/categoria.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './categoria.component.html',
+  styleUrl: './categoria.component.css'
+})
+export class CategoriaComponent {
+
+}
