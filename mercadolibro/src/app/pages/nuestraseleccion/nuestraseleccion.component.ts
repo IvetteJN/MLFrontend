aeeae7cee8e7c2973e7c2d599d3d8aea98f61a65
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-09 21:20:27 -0300

    reordeno componentes padre e hijo de 'nuestraseleccion'

--- mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
+++ mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CarruselComponent } from '../carrusel/carrusel.component';
+import { NuestraseleccionCarruselComponent } from './nuestraseleccion-carrusel/nuestraseleccion-carrusel.component';
 
 @Component({
   selector: 'app-nuestraseleccion',
   standalone: true,
-  imports: [CarruselComponent],
+  imports: [NuestraseleccionCarruselComponent],
   templateUrl: './nuestraseleccion.component.html',
   styleUrl: './nuestraseleccion.component.css'
 })
