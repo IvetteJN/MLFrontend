3b02ff0542e5f68fd11906dd876825fc512509f2
Franco Orellana <francoR_orellana@outlook.com>
2024-05-10 20:19:03 -0300

    Se actualiza rama con componente inicio

--- mercadolibro/src/app/app.routes.ts
+++ mercadolibro/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NuestraseleccionComponent } from './pages/nuestraseleccion/nuestraseleccion.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
-    {path: 'nuestraseleccion',component:NuestraseleccionComponent}
+    {path: 'nuestraseleccion',component:NuestraseleccionComponent},
+    {path: 'inicio', title: 'Login', component:InicioComponent}
 ];
