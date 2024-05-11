7908e8cefe17af97b540fa65fb0e91874dbcfa5a
PELLICER PALACIOS ANALIA ANAHI <analiapellicer86@gmail.com>
2024-05-11 10:57:29 -0300

    se incorporó las rutas en el nav

--- mercadolibro/src/app/app.routes.ts
+++ mercadolibro/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { NuestraseleccionComponent } from './pages/nuestraseleccion/nuestraseleccion.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { QuienesComponent } from './pages/quienes/quienes.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-    {path: 'nuestraseleccion',component:NuestraseleccionComponent}
+    {path: 'nuestraseleccion',component:NuestraseleccionComponent},
+    {path: 'contacto',component:ContactoComponent},
+    {path: 'quienes',component:QuienesComponent},
+    {path: 'landing',component:LandingComponent},
 ];
