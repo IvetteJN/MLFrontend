d6a1ee0e4b1a3c97507f7ea12498e3b4fa6ebe0e
Ivette Nobilta <ivette.nobilta@hotmail.comgit config --global user.name Ivette>
2024-05-13 16:56:35 -0300

    fusión de componentes

--- mercadolibro/src/app/pages/inicio/inicio.component.ts
+++ mercadolibro/src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
@@ -14,11 +13,11 @@ export class InicioComponent {
 
   //Elementos del login
   loginFormulario = new FormGroup({
-    usuario: new FormControl('',Validators.required),
-    contrasenia: new FormControl('',Validators.required),
+    usuario: new FormControl('', Validators.required),
+    contrasenia: new FormControl('', Validators.required),
   })
-  
-  enviarDatosLogin():void{
+
+  enviarDatosLogin(): void {
     console.log(this.loginFormulario.value);
   }
 
@@ -26,35 +25,35 @@ export class InicioComponent {
     return this.loginFormulario.get('usuario');
   }
 
-  get Contrasenia(){
+  get Contrasenia() {
     return this.loginFormulario.get('contrasenia');
   }
- //Elementos del registro
+  //Elementos del registro
   registroFormulario = new FormGroup({
-    usuarioRegistro: new FormControl('',Validators.required),
-    email: new FormControl('',Validators.required),
-    contraseniaRegistro: new FormControl('',Validators.required),
-    repetirContrasenia: new FormControl('',Validators.required),
+    usuarioRegistro: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    contraseniaRegistro: new FormControl('', Validators.required),
+    repetirContrasenia: new FormControl('', Validators.required),
   })
 
-  enviarDatosRegistro():void{
+  enviarDatosRegistro(): void {
     console.log(this.registroFormulario.value);
   }
-/*
-  get UsuarioRegistro(){
-    return this.registroFormulario.get('usuarioRegistro');
-  }
-
-  get Email(){
-    return this.registroFormulario.get('email');
-  }
-
-  get ContraseniaUsuario(){
-    return this.registroFormulario.get('contraseniaRegistro');
-  }
-
-  get RepetirContrasenia(){
-    return this.registroFormulario.get('repetirContrasenia');
-  }
-  */
+  /*
+    get UsuarioRegistro(){
+      return this.registroFormulario.get('usuarioRegistro');
+    }
+  
+    get Email(){
+      return this.registroFormulario.get('email');
+    }
+  
+    get ContraseniaUsuario(){
+      return this.registroFormulario.get('contraseniaRegistro');
+    }
+  
+    get RepetirContrasenia(){
+      return this.registroFormulario.get('repetirContrasenia');
+    }
+    */
 }
