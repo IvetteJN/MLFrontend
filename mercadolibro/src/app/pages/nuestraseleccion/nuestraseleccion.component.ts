fbd95af8ae428d038bfb10bfa0e121375c2759a5
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-05-13 14:58:36 -0300

    .

--- mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
+++ mercadolibro/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NuestraseleccionCarruselComponent } from './nuestraseleccion-carrusel/nuestraseleccion-carrusel.component';
+import { CarritoComponent } from './carrito/carrito.component';
+
 
 @Component({
   selector: 'app-nuestraseleccion',
   standalone: true,
-  imports: [NuestraseleccionCarruselComponent],
+  imports: [NuestraseleccionCarruselComponent, CarritoComponent],
   templateUrl: './nuestraseleccion.component.html',
   styleUrl: './nuestraseleccion.component.css'
 })
