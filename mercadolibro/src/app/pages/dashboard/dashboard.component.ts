06d7711ca82264501f0ba4e0433fffe85d0dbc81
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-19 18:12:58 -0300

    método servicio  get by id

--- mercadolibro/src/app/pages/dashboard/dashboard.component.ts
+++ mercadolibro/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../services/cliente.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,8 +9,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  private readonly usuariosSvc = inject (ClienteService)
+export class DashboardComponent implements OnInit {
   usuarios$ = this.usuariosSvc.getUsuarios();
+  usuarioSeleccionado: any; // Detalles del usuario seleccionado
+
+  constructor(private usuariosSvc: ClienteService) {}
+
+  ngOnInit(): void {
+    // Supongamos que obtienes el ID del usuario desde algún lugar (por ejemplo, una ruta)
+    const userId = 1; // ID de ejemplo
+
+    // Obtén el usuario por su ID
+    this.usuariosSvc.getUsuarioPorId(userId).subscribe(
+      (userDetails: any) => {
+        this.usuarioSeleccionado = userDetails;
+      },
+      (error) => {
+        console.error('Error al obtener el usuario:', error);
+      }
+    );
+  }
 }
 
