fc19c9075d1cf69a86a1f1465e47f3eb1b40818b
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-19 14:26:57 -0300

    logro traer datos de todos los usurarios al dashboard desde el json

--- mercadolibro/src/app/pages/dashboard/dashboard.component.ts
+++ mercadolibro/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ClienteService } from '../../services/cliente.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  private readonly usuariosSvc = inject (ClienteService)
+  usuarios$ = this.usuariosSvc.getUsuarios();
 }
+
