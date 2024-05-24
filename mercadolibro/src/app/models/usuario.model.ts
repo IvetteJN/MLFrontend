f5d380a5231cc8fe4f3cbb058cd030d7cfab4bd8
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-05-24 17:29:30 -0300

    dashboard/usuario datos

---
+++ mercadolibro/src/app/models/usuario.model.ts
@@ -0,0 +1,6 @@
+export interface usuario {
+    id: number;
+    nombre: string;
+    email: string;
+    password: string;
+}
--- mercadolibro/src/app/pages/dashboard/dashboardlanding/dashboardlanding.component.ts
+++ mercadolibro/src/app/pages/dashboard/dashboardlanding/dashboardlanding.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { usuario } from '../../../models/usuario.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboardlanding',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './dashboardlanding.component.html',
   styleUrl: './dashboardlanding.component.css'
 })
 export class DashboardlandingComponent {
+  usuario: usuario = {
+    id: 1,
+    nombre: 'marcelo',
+    email: 'a@a.com',
+    password: '1234',
 
+  }
 }
