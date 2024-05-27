83ebc45fd72df678da94e22565a047263f9d35cc
Franco Orellana <francoR_orellana@outlook.com>
2024-05-27 19:02:04 -0300

    Cambios en promociones

--- mercadolibro/src/app/services/promociones.service.ts
+++ mercadolibro/src/app/services/promociones.service.ts
@@ -21,10 +21,10 @@ export class PromocionesService {
     return '20% descuento exclusivo para miembros de Club La Voz, en compras mayores a 20.000 pesos. Promoción valida hasta el 31/12/2024 inclusive.'
   }
   informacionOferta4():string{
-    return 'Te damos la bienvenida a mercadolibro, disfruta de una compra hasta 100.000 pesos con 20% de descuento. Utiliza el cupón al momento de la compra para obtener el beneficio.'
+    return 'Envió gratis a domicilio o puntos de entrega en cualquier ciudad dentro del territorio nacional que se encuentre dentro de la red de entrega de Correo Argentino. Solo aplicable en compras con un valor igual o superior a 40.000 pesos. Promoción valida hasta el 31/12/2024 inclusive.'
   }
   informacionOferta5():string{
-    return 'Aprovecha la oportunidad de comprar el libro que siempre soñaste con un descuento del 25%. Solo valido para usuarios que hayan realizado  compras por un monto mayor a 20.000 pesos. Promoción valida hasta el 31/12/2024 inclusive.'
+    return 'Aprovecha la oportunidad de comprar  tus novelas favoritas con un descuento del 15%. Descuento aplicable a todos los libros que se encuentren dentro de la categoría novelas. Promoción valida hasta el 30/06/2024 inclusive.'
   }
   informacionOferta6():string{
     return 'Aprovecha todas las semanas de comprar un libro de nuestra selección con un increíble descuento. No dejes pasar esta oportunidad de llevarte a tu casa clásicos de la literatura, best seller y mucha mas. Promoción valida hasta el 09/06/2024 inclusive.'
