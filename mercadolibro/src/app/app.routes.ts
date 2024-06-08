03aa376dc780e666d875581b5d9a78a722bc3f20
nahir1009 <nahir1009@gmail.com>
2024-06-08 20:55:51 -0300

    pasarela de pagos terminada

--- mercadolibro/src/app/app.routes.ts
+++ mercadolibro/src/app/app.routes.ts
@@ -18,7 +18,7 @@ import { PromocionesComponent } from './pages/dashboard/promociones/promociones.
 import { ReviewsComponent } from './pages/dashboard/reviews/reviews.component';
 import { ResumenCompraComponent } from './pages/dashboard/finalizar-compra/finalizar-compra.component';
 import { AuthGuard } from './guard/auth.guard';
-import { PasarelaComponent } from './pages/dashboard/pasarela/pasarela.component';
+
 
 export const routes: Routes = [
     { path: 'landing', title: 'Home', component: LandingComponent },
@@ -40,8 +40,6 @@ export const routes: Routes = [
             { path: 'promociones', component: PromocionesComponent, title: 'Descuentos y cupones' },
             { path: 'calificacion', component: ReviewsComponent, title: 'Calificacion de Productos' },
             { path: 'resumenCompra', component: ResumenCompraComponent, title: 'Finalizar compra' },
-            { path: 'pasarela', component: PasarelaComponent, title: 'pasarela de pagos' }
-
         ]
     },
     { path: '**', component: Pagina404Component }
